da9178a00bb4deff716b23add8871930390041c8
Emi <emilianonalvarez@gmail.com>
2025-04-10 18:45:22 -0300

    ejercicio1

---
+++ Ejercicios1/secuenciales.py
@@ -0,0 +1,61 @@
+print ("Hola Mundo") 
+
+
+Emi = 15
+
+Nico = "Amigo"
+
+Emi = "Enemigo"
+
+Emi = 8 + 9 
+
+NOMBRE = input ("Como es tu nombre?")
+ 
+print  (NOMBRE)
+
+print (Emi, Nico)
+
+numero_entero = input ("Ingrese un numero ")
+
+numero_entero =int(numero_entero)
+
+print (f"El numero ingresado es  {numero_entero}")
+
+numero_decimal = input ("Ingrese un numero decimal")
+
+numero_decimal = float(numero_decimal)
+
+print (f"El numero ingresado es {numero_decimal}")
+
+Suma = numero_entero + numero_decimal 
+
+print (f"tu numero de suma  es   {Suma}")
+
+#CALCULAR   EL AREA DEL CIRCULO
+
+radio = 6
+pi = 3.14159
+
+area = pi * radio* radio 
+
+print  (f"El area del circulo es {area}")
+
+
+#calcular edad 
+
+anio_nacimiento = int(input ("Ingrese su anio de nacimiento" ))
+anio_actual = 2025 
+
+edad = anio_actual - anio_nacimiento
+
+print (f"Su edad es {edad}")
+
+
+precio = 19.99
+cantidad =3
+factura = precio * cantidad
+print (f"la factura es de {factura}")
+
+nombre = "ana"
+edad = 25 
+print (f"{nombre} tiene {edad} años.")
