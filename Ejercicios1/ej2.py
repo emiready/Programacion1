5b85d8918528b3e299dd5b88ebcdf0f171c68bb4
Emi <emilianonalvarez@gmail.com>
2025-04-14 07:37:14 -0300

    agrego variable apellido

--- Ejercicios1/ej2.py
+++ Ejercicios1/ej2.py
@@ -2,7 +2,7 @@
 
 
 nombre = input ("Me decis tu nombre por favor")
-apeliido = input ("Me decis tu apellido por favor")
+apellido = input ("Me decis tu apellido por favor")
 
 print(f"mi nombre es{nombre} y tu {apellido}")
 
